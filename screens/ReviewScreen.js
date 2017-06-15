6fd52ae5b243acde9797c5ab04b533008d496f81
Gerald <futuregerald@ninjasitm.com>
2017-06-14 21:47:53 -0400

    first real commit

---
+++ screens/ReviewScreen.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
+
+class ReviewScreen extends Component {
+    // destructer navigate from navigation
+    static navigationOptions = ({ navigation }) => ({
+        title: 'Review Jobs',
+        headerRight: <Button title="Settings" onPress={()=> navigation.navigate('settings') }/>
+    })
+
+    render(){
+        return (
+            <View>
+                <Text>ReviewScreen</Text>
+                <Text>ReviewScreen</Text>
+                <Text>ReviewScreen</Text>
+                <Text>ReviewScreen</Text>
+                <Text>ReviewScreen</Text>
+                <Text>ReviewScreen</Text>
+            </View>
+        );
+    }
+}
+
+export default ReviewScreen;
---
+++ screens/WelcomeScreen.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class WelcomeScreen extends Component {
+    render(){
+        return (
+            <View>
+                {console.log('hope this works for the welcome screen')}
+                <Text>WelcomeScreen</Text>
+                <Text>WelcomeScreen</Text>
+                <Text>WelcomeScreen</Text>
+                <Text>WelcomeScreen</Text>
+                <Text>WelcomeScreen</Text>
+                <Text>WelcomeScreen</Text>
+            </View>
+        );
+    }
+}
+
+export default WelcomeScreen;
